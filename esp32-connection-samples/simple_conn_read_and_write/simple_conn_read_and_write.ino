e210fd4542b3fe1dcf8b5061af5814a689530d26
Arne Lender <128963447+al83012@users.noreply.github.com>
2026-04-03 23:07:13 +0200

    Added new code for the sample:
    Now with status LED to better monitor the current connection state; On this board it was pin 48, but it required the solder bridge on the board to be closed.

--- esp32-connection-samples/simple_conn_read_and_write/simple_conn_read_and_write.ino
+++ esp32-connection-samples/simple_conn_read_and_write/simple_conn_read_and_write.ino
@@ -1,7 +1,27 @@
 #include <ArduinoWebsockets.h>
 #include <WiFi.h>
 
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN 48
+#define NUM_LEDS 1
+
+#define C_SEARCH_WIFI pixel.Color(0, 100, 200)
+#define C_WIFI_FOUND pixel.Color(0, 255, 255)
+#define C_CONNECT pixel.Color(255, 255, 0)
+#define C_CONNECT_FAIL pixel.Color(255, 200, 0)
+#define C_CLOSED pixel.Color(255, 0, 0)
+#define C_PING pixel.Color(0, 0, 255)
+#define C_CONNECT_OPEN pixel.Color(0, 255, 0)
+#define C_OFF pixel.Color(0, 0, 0)
+#define C_MSG pixel.Color(255, 255, 255)
+
+
+Adafruit_NeoPixel pixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 using namespace websockets;
+
+
 
 String websockets_server = "ws://";
 uint16_t port = 9001;
@@ -12,17 +32,32 @@ unsigned long lastReconnectAttempt = 0;
 const unsigned long reconnectInterval = 2000;
 
 void onMessageCallback(WebsocketsMessage message) {
+  pixel.setPixelColor(0, C_MSG);
+  pixel.show();
   Serial.print("Got Message: ");
   Serial.println(message.data());
+  delay(50);
+  pixel.setPixelColor(0, C_OFF);
+  pixel.show();
 }
 
 void onEventsCallback(WebsocketsEvent event, String data) {
   if (event == WebsocketsEvent::ConnectionOpened) {
+    pixel.setPixelColor(0, C_CONNECT_OPEN);
+    pixel.show();
     Serial.println("Connection Opened");
   } else if (event == WebsocketsEvent::ConnectionClosed) {
+    pixel.setPixelColor(0, C_CLOSED);
+    pixel.show();
     Serial.println("Connection Closed");
   } else if (event == WebsocketsEvent::GotPing) {
     Serial.println("Ping " + data);
+    pixel.setPixelColor(0, C_PING);
+    pixel.show();
+    
+    delay(100);
+    pixel.setPixelColor(0, C_OFF);
+    pixel.show();
     
     //client.pong(data);
   } else if (event == WebsocketsEvent::GotPong) {
@@ -37,8 +72,16 @@ void initWiFi() {
   Serial.print("Connecting to WiFi ..");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print('.');
-    delay(1000);
+    pixel.setPixelColor(0, C_SEARCH_WIFI);
+    pixel.show();
+    delay(200);
+    pixel.setPixelColor(0, C_OFF);
+    pixel.show();
+    delay(800);
   }
+
+  pixel.setPixelColor(0, C_WIFI_FOUND);
+  pixel.show();
 
   WiFi.setAutoReconnect(true);
   WiFi.persistent(true);
@@ -67,12 +110,23 @@ void connectWS() {
     Serial.println("Connected!");
     client.send("Hi Server!");
     client.ping();
+    pixel.setPixelColor(0, C_CONNECT);
+    pixel.show();
   } else {
+    pixel.setPixelColor(0, C_CONNECT_FAIL);
+    pixel.show();
+    delay(10);
+    pixel.setPixelColor(0, C_OFF);
+    pixel.show();
     Serial.println("Connection failed");
   }
 }
 
 void setup() {
+  pixel.begin();
+  pixel.clear();
+  pixel.show();
+
   Serial.begin(115200);
   initWiFi();
   
@@ -82,6 +136,7 @@ void setup() {
 
   client.onMessage(onMessageCallback);
   client.onEvent(onEventsCallback);
+
 
   connectWS();
 }
@@ -98,7 +153,12 @@ void loop() {
     lastPing = millis();
   }*/
 
+  if(!WiFi.isConnected()) {
+    initWiFi();
+  }
+
   if (!client.available()) {
+    //pixel.setPixelColor(0, C_CONNECT_FAIL);
     unsigned long now = millis();
 
     if (now - lastReconnectAttempt > reconnectInterval) {
