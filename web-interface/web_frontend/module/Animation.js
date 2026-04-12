444cb6154efb021ff01d205b91d522cfeaa14884
Alexander Sitzmann <82871780+DAOCMINECRAFT@users.noreply.github.com>
2026-04-12 12:21:58 +0200

    first half of the path change animation complete

--- web-interface/web_frontend/module/Animation.js
+++ web-interface/web_frontend/module/Animation.js
@@ -292,6 +292,35 @@ export class AnimBorderColor extends Animation {
     }
 }
 
+export class AnimCssChange extends Animation {
+    constructor(duration, object, items, replacement) {
+        super(duration, object);
+        this.items = items;
+        this.replacement = replacement;
+    }
+
+    execute() {
+        if (this.finished) {
+            return;
+        }
+        if (this.duration === this.remaining_duration) {
+            for (let i = 0; i < this.items.length; i++) {
+                if (this.object.className.includes(" " + this.items[i])) {
+                    this.object.className = this.object.className.replace(" " + this.items[i], " " + this.replacement);
+                    break;
+                }
+            }
+
+        }
+
+        this.remaining_duration--;
+
+        if (this.remaining_duration < 0) {
+            this.finished = true;
+        }
+    }
+}
+
 export class AnimationHandler {
     animations = [];
     rep_animations = [];
@@ -377,7 +406,7 @@ export class AnimGroup extends Animation {
         }
 
         let amount = Math.min(Math.floor(this.current_delay/this.delay), this.animations.length);
-        let all_finished = (amount >= this.animations.length - 1);
+        let all_finished = (amount >= this.animations.length);
 
         for (let i = 0; i < amount; i++) {
             if (!(this.animations[i].finished)) {
@@ -392,4 +421,101 @@ export class AnimGroup extends Animation {
 
         this.current_delay++;
     }
+}
+
+export function generatePathAnimGroup(path/*: int[][]*/, tiles) { //tiles are all arms tied to respective coords
+    const anim_time = 5;
+    let c_group = new AnimGroup(0);
+    let was_change = false;
+    let changed_group;
+    let changed_group_time = 0;
+
+    for (let i = path.length - 1; i >= 0; i--) {
+        let part = path[i];
+        let type = part.pop() //0: same ; -1: remove ; +1: add
+
+        let duration = 0; //add 5 to the end to make it seamless
+        for (let j = 0; j < part.length; j+=2) {
+            duration += tiles[[part[j], part[j+1]]].length*anim_time;
+        }
+        duration /= 3;
+        duration -= 10; //remove the last few child_times
+
+        /*console.log("type:" + type)
+        console.log("duration:" + duration)
+        console.log(part)*/
+
+        let group = new AnimGroup(anim_time/3);
+        let n_group;
+
+        switch (type) {
+            case 0:
+                for (let j = 0; j < part.length; j+=2) { //loop trough all coords in +2 jumps
+                    for (let k = 0; k < tiles[[part[j], part[j+1]]].length; k++) {//for all arms in tiles for the coords
+                        group.add(new AnimCssChange(anim_time,tiles[[part[j], part[j+1]]][k], ["on","repl"], "highlight"));
+                        //tiles[[part[j], part[j+1]]][k].style.opacity = 0;
+                        //FIXED: Bug where the last for animations are created but not executed
+                        //fix: cant be displayed on page load, only with a timeout of 1000
+                    }
+                }
+                n_group = new AnimGroup(duration);
+                n_group.add(group);
+                n_group.add(c_group);
+                c_group = n_group;
+                break;
+            case 1:
+                for (let j = 0; j < part.length; j+=2) { //loop trough all coords in +2 jumps
+                    for (let k = 0; k < tiles[[part[j], part[j+1]]].length; k++) {//for all arms in tiles for the coords
+                        group.add(new AnimCssChange(anim_time,tiles[[part[j], part[j+1]]][k], ["on","repl"], "add"));
+                    }
+                }
+                if (!was_change) {
+                    changed_group = group;
+                    changed_group_time = duration;
+                    was_change = true;
+                } else {
+                    let adder_group = new AnimGroup(0);
+                    adder_group.add(group);
+                    adder_group.add(changed_group);
+                    n_group = new AnimGroup(Math.max(changed_group_time, duration));
+                    n_group.add(adder_group);
+                    n_group.add(c_group);
+                    c_group = n_group;
+                    was_change = false;
+                }
+                break;
+            case -1:
+                for (let j = 0; j < part.length; j+=2) { //loop trough all coords in +2 jumps
+                    for (let k = 0; k < tiles[[part[j], part[j+1]]].length; k++) {//for all arms in tiles for the coords
+                        group.add(new AnimCssChange(anim_time,tiles[[part[j], part[j+1]]][k], ["on","repl"], "remove"));
+                    }
+                }
+                if (!was_change) {
+                    changed_group = group;
+                    changed_group_time = duration;
+                    was_change = true;
+                } else {
+                    let adder_group = new AnimGroup(0);
+                    adder_group.add(group);
+                    adder_group.add(changed_group);
+                    n_group = new AnimGroup(Math.max(changed_group_time, duration));
+                    n_group.add(adder_group);
+                    n_group.add(c_group);
+                    c_group = n_group;
+                    was_change = false;
+                }
+                break;
+        }
+
+
+    }
+
+    if (was_change) {
+        let n_group = new AnimGroup(changed_group_time);
+        n_group.add(changed_group);
+        n_group.add(c_group);
+        c_group = n_group;
+    }
+
+    return c_group;
 }
--- web-interface/web_frontend/module/index.js
+++ web-interface/web_frontend/module/index.js
@@ -12,7 +12,8 @@ import {
     AnimFadeOut,
     AnimBackgroundColor,
     AnimSlideColor,
-    AnimGroup
+    AnimGroup,
+    generatePathAnimGroup
 } from "./Animation.js"
 import { StyleAdder } from "./style_adder.js";
 
@@ -56,16 +57,16 @@ export class Index {
 
         //creating the AnimationHandler
         Index.animHandler = new AnimationHandler();
-        window.setInterval(() => {Index.animHandler.nextFrame();}, 10)
         window.setInterval(() => {
             let request = new XMLHttpRequest();
             request.addEventListener("load", function () {Index.handleUpdate(JSON.parse(this.responseText));});
             request.open("GET", document.location.origin + "/update", true);
             request.send();
         }, 100);
+        window.setInterval(() => {Index.animHandler.nextFrame();}, 10)
 
-
-        init_maze();
+        //TODO: remove timeout in production
+        window.setTimeout(() => init_maze(), 1000); //the timeout is only for the test animation which had a bug
 
 
         //let borderAnim = new AnimBorderColor(100, Index.squares[[5, 5]], "darkblue", "cyan");
@@ -110,7 +111,7 @@ export class Index {
     }
 
     static buttonStartStop() {
-        play_reset_animation();
+        //play_reset_animation();
 
         let request = new XMLHttpRequest();
         request.open("POST", document.location.origin + "/action");
@@ -176,36 +177,177 @@ function init_maze() {
         tile.appendChild(arm_container);
 
         let node = document.createElement("div");
-        node.className = "sys-arm_node";
+        node.className = "sys-arm_node repl";
         node.id = "sys-arm_node_" + i;
         arm_container.appendChild(node);
 
         if (!(coords[1] === 0)) {
             let arm_up = document.createElement("div");
-            arm_up.className = "sys-arm_arm sys-arm_arm-n";
+            arm_up.className = "sys-arm_arm sys-arm_arm-n repl";
             arm_up.id = "sys-arm_arm-n_" + i;
             arm_container.appendChild(arm_up);
         }
 
         if (!(coords[1] === 15)) {
             let arm_down = document.createElement("div");
-            arm_down.className = "sys-arm_arm sys-arm_arm-s";
+            arm_down.className = "sys-arm_arm sys-arm_arm-s repl";
             arm_down.id = "sys-arm_arm-s_" + i;
             arm_container.appendChild(arm_down);
         }
 
         if (!(coords[0] === 0)) {
             let arm_left = document.createElement("div");
-            arm_left.className = "sys-arm_arm sys-arm_arm-w";
+            arm_left.className = "sys-arm_arm sys-arm_arm-w repl";
             arm_left.id = "sys-arm_arm-w_" + i;
             arm_container.appendChild(arm_left);
         }
 
         if (!(coords[0] === 15)) {
             let arm_right = document.createElement("div");
-            arm_right.className = "sys-arm_arm sys-arm_arm-e";
+            arm_right.className = "sys-arm_arm sys-arm_arm-e repl";
             arm_right.id = "sys-arm_arm-e_" + i;
             arm_container.appendChild(arm_right);
+        }
+    }
+
+    //let animation = generatePathAnimGroup([[0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 5, 1, 5, 2, 5, 3, 5, 4, 5, 5, 0]], tiles);
+    let animation = displayPathChange([
+        [0, 0, 1, 0, 2, 0, 3, 0, 4, 0, 5, 0, 5, 1, 5, 2, 5, 3, 5, 4, 5, 5, 0],
+        [6, 5, 7, 5, 8, 5, 9, 5, 9, 6, 9, 7, 9, 8, 1],
+        [5, 6, 5, 7, 5, 8, 5, 9, 6, 9, 7, 9, 8, 9, -1],
+        [9, 9, 9, 10, 10, 10, 0]
+    ]);
+    Index.animHandler.add(animation);
+}
+
+function displayPathChange(changed_path) {
+    let tiles = [];
+    let group_points = [];
+    for (let g = 0; g < changed_path.length; g++) {
+        group_points.push([changed_path[g][0], changed_path[g][1], changed_path[g][changed_path[g].length-1]]);
+        group_points.push([changed_path[g][changed_path[g].length-3], changed_path[g][changed_path[g].length-2],
+            changed_path[g][changed_path[g].length-1]]);
+
+        let group = changed_path[g];
+        let prev = [group[0], group[1]];
+        tiles[[prev[0], prev[1]]] = [document.getElementById("sys-arm_node_" + co_crds_i([prev[0], prev[1]]))];
+        for (let i = 2; i < group.length -1; i+=2) { //-1 to skip the group type
+            if (prev[0] < group[i]) {
+                tiles[[prev[0], prev[1]]].push(document.getElementById("sys-arm_arm-e_" + co_crds_i([prev[0], prev[1]])));
+                tiles[[group[i], group[i+1]]] = [
+                    document.getElementById("sys-arm_arm-w_" + co_crds_i([group[i], group[i+1]])),
+                    document.getElementById("sys-arm_node_" + co_crds_i([group[i], group[i+1]]))
+                ];
+            }
+            else if (prev[0] > group[i]) {
+                tiles[[prev[0], prev[1]]].push(document.getElementById("sys-arm_arm-w_" + co_crds_i([prev[0], prev[1]])));
+                tiles[[group[i], group[i+1]]] = [
+                    document.getElementById("sys-arm_arm-e_" + co_crds_i([group[i], group[i+1]])),
+                    document.getElementById("sys-arm_node_" + co_crds_i([group[i], group[i+1]]))
+                ];
+            }
+            else if (prev[1] < group[i+1]) {
+                tiles[[prev[0], prev[1]]].push(document.getElementById("sys-arm_arm-s_" + co_crds_i([prev[0], prev[1]])));
+                tiles[[group[i], group[i+1]]] = [
+                    document.getElementById("sys-arm_arm-n_" + co_crds_i([group[i], group[i+1]])),
+                    document.getElementById("sys-arm_node_" + co_crds_i([group[i], group[i+1]]))
+                ];
+            }
+            else if (prev[1] > group[i+1]) {
+                tiles[[prev[0], prev[1]]].push(document.getElementById("sys-arm_arm-n_" + co_crds_i([prev[0], prev[1]])));
+                tiles[[group[i], group[i+1]]] = [
+                    document.getElementById("sys-arm_arm-s_" + co_crds_i([group[i], group[i+1]])),
+                    document.getElementById("sys-arm_node_" + co_crds_i([group[i], group[i+1]]))
+                ];
+            }
+            prev[0] = group[i];
+            prev[1] = group[i+1];
+        }
+    }
+
+    for (let i = 0; i < group_points.length; i++) {
+        for (let j = i+1; j < group_points.length; j++) {
+            if (math_pos(group_points[i][0]-group_points[j][0]) <= 1 &&
+                math_pos(group_points[i][1]-group_points[j][1]) <= 1 &&
+                !(math_pos(group_points[i][0]-group_points[j][0]) === 1 &&
+                    math_pos(group_points[i][1]-group_points[j][1]) === 1)) {
+
+                let prev = [group_points[i][0], group_points[i][1]];
+
+                if (prev[0] < group_points[j][0]) {
+                    if (group_points[j][2] === 0) {
+                        tiles[prev].push(document.getElementById("sys-arm_arm-e_" + co_crds_i([prev[0], prev[1]])));
+                        tiles[prev].push(
+                            document.getElementById("sys-arm_arm-w_" + co_crds_i([group_points[j][0], group_points[j][1]])));
+                    } else {
+                        tiles[[group_points[j][0], group_points[j][1]]]
+                            .push(document.getElementById("sys-arm_arm-e_" + co_crds_i([prev[0], prev[1]])));
+                        tiles[[group_points[j][0], group_points[j][1]]].push(
+                            document.getElementById("sys-arm_arm-w_" + co_crds_i([group_points[j][0], group_points[j][1]])));
+                    }
+                }
+                else if (prev[0] > group_points[j][0]) {
+                    if (group_points[j][2] === 0) {
+                        tiles[prev].push(document.getElementById("sys-arm_arm-w_" + co_crds_i([prev[0], prev[1]])));
+                        tiles[prev].push(
+                            document.getElementById("sys-arm_arm-e_" + co_crds_i([group_points[j][0], group_points[j][1]])));
+                    } else {
+                        tiles[[group_points[j][0], group_points[j][1]]]
+                            .push(document.getElementById("sys-arm_arm-w_" + co_crds_i([prev[0], prev[1]])));
+                        tiles[[group_points[j][0], group_points[j][1]]].push(
+                            document.getElementById("sys-arm_arm-e_" + co_crds_i([group_points[j][0], group_points[j][1]])));
+                    }
+                }
+                else if (prev[1] < group_points[j][1]) {
+                    if (group_points[j][2] === 0) {
+                        tiles[prev].push(document.getElementById("sys-arm_arm-s_" + co_crds_i([prev[0], prev[1]])));
+                        tiles[prev].push(
+                            document.getElementById("sys-arm_arm-n_" + co_crds_i([group_points[j][0], group_points[j][1]])));
+                    } else {
+                        tiles[[group_points[j][0], group_points[j][1]]]
+                            .push(document.getElementById("sys-arm_arm-s_" + co_crds_i([prev[0], prev[1]])));
+                        tiles[[group_points[j][0], group_points[j][1]]].push(
+                            document.getElementById("sys-arm_arm-n_" + co_crds_i([group_points[j][0], group_points[j][1]])));
+                    }
+                }
+                else if (prev[1] > group_points[j][1]) {
+                    if (group_points[j][2] === 0) {
+                        tiles[prev].push(document.getElementById("sys-arm_arm-n_" + co_crds_i([prev[0], prev[1]])));
+                        tiles[prev].push(
+                            document.getElementById("sys-arm_arm-s_" + co_crds_i([group_points[j][0], group_points[j][1]])));
+                    } else {
+                        tiles[[group_points[j][0], group_points[j][1]]]
+                            .push(document.getElementById("sys-arm_arm-n_" + co_crds_i([prev[0], prev[1]])));
+                        tiles[[group_points[j][0], group_points[j][1]]].push(
+                            document.getElementById("sys-arm_arm-s_" + co_crds_i([group_points[j][0], group_points[j][1]])));
+                    }
+                }
+            }
+        }
+    }
+
+    return generatePathAnimGroup(changed_path, tiles);
+}
+
+//deprecated function
+//moved function to backend and awaiting server protocol to rewrite the function
+//according to data received from server
+function dep_displayPathChange(path_old, path_new) {
+    let groups = [];
+    let c_group = [];
+    let type = -2;
+    for (let i = 0; i < path_old.length; i+=2) {
+        if (path_old[i] === path_new[i] && path_old[i+1] === path_new[i+1]) {
+            if (!(type === -2 || type === 0)) {
+                c_group.push(type);
+                groups.push(c_group);
+                c_group = [];
+            }
+            c_group.push(path_new[i]);
+            c_group.push(path_new[i+1]);
+            type = 0;
+        } else if (path_old[i] !== path_new[i] || path_old[i+1] !== path_new[i+1]) {
+            if (!(type === -2 || type === -1 || type === 1)) {}
         }
     }
 }
@@ -334,6 +476,8 @@ function convert_index_to_coords(i) {
     return [x,y];
 }
 
+//just an alias function
+function co_crds_i(i) {return convert_coords_to_index(i);}
 function convert_coords_to_index(i) {
     return i[1]*16+i[0];
 }
@@ -430,4 +574,8 @@ function add_algorithm(algorithm) {
         Index.closePopAlgo();
     });
     document.getElementById("algo_content").appendChild(algorithm_ele);
+}
+
+function math_pos(x) {
+    return x*(x<0?-1:1);
 }
