26f6071a2e738847d528140b8644567b86cc4ec9
Arne Lender <128963447+al83012@users.noreply.github.com>
2026-05-28 16:28:52 +0200

    Wrote shorted Process-Test (with Micromouse- and Frontend Simulators)

---
+++ backend-micromouse/src/tests/process_test_short.rs
@@ -0,0 +1,44 @@
+use tracing::{error, span, Instrument};
+
+use crate::{
+    process::Process,
+    tests::{
+        frontend_simulator::{self, FrontendSimulator},
+        micromouse_simulator::{self, MicromouseSimulator},
+    },
+    utils::hyperlink_logging::{init_tree_logger, process_span},
+};
+
+fn test_process_short() {
+    const N: usize = super::TEST_MAP_SIZE;
+    init_tree_logger();
+
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    let m_handle = async {
+        let world = super::test_map(0.5);
+        let mut micromouse_simulator = MicromouseSimulator::new(world);
+        micromouse_simulator.run(3).await;
+    }
+    .instrument(process_span("micromouse_simulator"));
+
+    let f_handle = async {
+        let mut frontend_simulator = FrontendSimulator::new();
+        frontend_simulator.run().await;
+    }
+    .instrument(process_span("frontend_simulator"));
+
+    let p_handle = async {
+        let process: Process<N> = Process::new().await.expect("Process creation failed");
+        process.run().await;
+    }
+    .instrument(process_span("frontend_simulator"));
+
+    rt.block_on(async {
+        tokio::select! {
+            _ = m_handle => {error!(target: "tests", "Micromouse Simulator failed")}
+            _ = f_handle => {error!(target: "tests", "Frontend Simulator failed")}
+            _ = p_handle => {error!(target: "tests", "Process failed")}
+        }
+    });
+}
