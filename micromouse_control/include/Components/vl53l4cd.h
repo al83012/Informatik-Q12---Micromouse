5897c7e32df345b144a898afdb2eacbaea3cfe6b
Daniel B. <83012822+EarlCereal@users.noreply.github.com>
2026-07-28 14:46:15 +0200

    Delete old micromouse_control directory for update

--- micromouse_control/include/Components/vl53l4cd.h
+++
@@ -1,7 +0,0 @@
-#ifndef VL53L4CD_H
-#define VL53L4CD_H
-#include <Wire.h>
-
-// Time-of-Flight (ToF) Distance Sensor
-
-#endif
--- micromouse_control/src/Components/vl53l4cd.cpp
+++
@@ -1,2 +0,0 @@
-#include "Components/VL53L4CD.h"
-// Time-of-Flight (ToF) Distance Sensor
